d54be52c5789056f927f1479002bbe1f9aef43d1
ashishverma94 <hariashish1@gmail.com>
2023-12-23 15:12:46 +0530

    connect to mongo database

---
+++ src/index.js
@@ -0,0 +1,46 @@
+import dotenv from 'dotenv' ;
+import connectDB from "./db/index.js" ;
+
+
+dotenv.config({
+    path:'./env'
+})
+
+
+connectDB( ) ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ;( async ()=>{
+//     try{
+//         await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+//         app.on("error", (error)=>{
+//             console.log("ERROR:",error) ;
+//             throw error
+//         })
+
+//         app.listen(process.env.PORT, ()=>{
+//             console.log('app is listening on PORT ${process.env.PORT}')
+//         })
+//     } catch( error ){
+//         console.log("error:", error )
+//         throw error ;
+//     }
+// })()
